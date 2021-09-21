7b442935d31c570510f84120311859db14bc9832
K-shir0 <kakishiro2190394@gmail.com>
2021-09-21 16:40:41 +0900

    add 投稿画面を作成

--- lib/ui/post_page.dart
+++ lib/ui/post_page.dart
@@ -9,7 +9,48 @@ class PostPage extends HookWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: [ElevatedButton(onPressed: () {}, child: const Text('登録'))],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              maxLines: null,
+              decoration: const InputDecoration(
+                hintText: 'いまどうしてる？',
+                border: InputBorder.none,
+                enabledBorder: InputBorder.none,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      side: BorderSide(color: Theme.of(context).primaryColor),
+                    ),
+                    onPressed: () {},
+                    child: const Text('キャンセル'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('登録'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
