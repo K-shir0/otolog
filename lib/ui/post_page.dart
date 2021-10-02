b6ea0f796d37e0b1807a68ba208c4226246c2b7f
K-shir0 <kakishiro2190394@gmail.com>
2021-10-02 21:40:47 +0900

    update: スクロールするようにした.

--- lib/ui/post_page.dart
+++ lib/ui/post_page.dart
@@ -19,91 +19,98 @@ class PostPage extends HookWidget {
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                height: 256,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fitWidth,
-                    image: NetworkImage('https://picsum.photos/200/300'),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                child: TextFormField(
-                  minLines: 3,
-                  maxLines: 3,
-                  decoration: const InputDecoration(
-                    hintText: 'キャプションを書く',
-                    border: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                  ),
-                ),
-              ),
-              Container(
-                height: 1,
-                color: Theme.of(context).dividerColor,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text('スレッド'),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      size: 16,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 1,
-                color: Theme.of(context).dividerColor,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text('タグ付け'),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            size: 16,
-                          ),
-                        ],
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 256,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fitWidth,
+                        image: NetworkImage('https://picsum.photos/200/300'),
                       ),
                     ),
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text(
-                          'フルコンボ',
-                          style: Theme.of(context).textTheme.caption,
+                  ),
+                  // TODO(k-shir0): スクロールがずれるので修正したい.
+                  Scrollbar(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                      child: TextFormField(
+                        minLines: 3,
+                        maxLines: 3,
+                        decoration: const InputDecoration(
+                          hintText: 'キャプションを書く',
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 1,
-                color: Theme.of(context).dividerColor,
+                    ),
+                  ),
+                  Container(
+                    height: 1,
+                    color: Theme.of(context).dividerColor,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 16, 8, 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('スレッド'),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 1,
+                    color: Theme.of(context).dividerColor,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text('タグ付け'),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              'フルコンボ',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 1,
+                    color: Theme.of(context).dividerColor,
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
